6d1681909c09f5af64fdd83c234ceacb6e8f0432
sheshanth <sheshanth_soccer@yahoo.com>
2023-03-23 20:12:01 +0530

    new apis

--- src/main/java/com/example/department/DepartmentApplication.java
+++ src/main/java/com/example/department/DepartmentApplication.java
@@ -1,11 +1,16 @@
 package com.example.department;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @EnableDiscoveryClient
 @SpringBootApplication
@@ -21,9 +26,25 @@ public class DepartmentApplication {
 @RequestMapping("/")
 class Greeting {
 
+	@Autowired
+	public DiscoveryClient discoveryClient;
+
+	@Value("${spring.application.name}")
+	private String appName;
+
 	@GetMapping
 	public String greetings() {
 		return "Hello!, from department application";
+	}
+
+	@GetMapping
+	public List<String> getAllServices() {
+		return this.discoveryClient.getServices();
+	}
+
+	@GetMapping("/department/app/name")
+	public String getAppName() {
+		return this.appName;
 	}
 
 }
